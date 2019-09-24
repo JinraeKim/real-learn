368f8644303257a09754ded068daabc3bc061dbc
Seong-hun Kim <kshoon92@gmail.com>
2019-09-24 21:01:39 +0900

    AEC 2020 - Initial commit
    Basic dynamics soaring environment was set.

---
+++ topics/safe-dynamic-soaring/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from envs.soaring import Env
+
+np.random.seed(1)
+
+env = Env(initial_state=np.array([0, 0, -5, 13, 0, 0]).astype('float'))
+
+time_step = 0.01
+time_series = np.arange(0, 2, time_step)
+
+obs = env.reset()
+obs_series = np.array([], dtype=np.float64).reshape(0, len(obs))
+
+for i in time_series:
+    controls = np.zeros(2)
+
+    # Need logging here
+    next_obs, reward, done, _ = env.step(controls)
+
+    if done:
+        break
+
+    obs = next_obs
+    obs_series = np.vstack((obs_series, obs))
