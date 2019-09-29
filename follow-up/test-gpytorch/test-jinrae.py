725eb0fa976075cb260cf495dbede0f5fcc1e33a
jinrae <jinrae>
2019-09-29 17:37:40 +0900

    WIP
    WIP

--- follow-up/test-gpytorch/test-jinrae.py
+++ follow-up/test-gpytorch/test-jinrae.py
@@ -54,9 +54,36 @@ for i in range(training_iter):
     # calc loss and backprop gradients
     loss = -mll(output, train_y)
     loss.backward()
-    print('Iter %d/%d - Loss: %.3f    lengthscale: %.3f    noise: %.3f' %
+    print('Iter %d/%d - Loss: %.3f    lengthscale: %.3f    noise: %.3f' % (
          i + 1, training_iter, loss.item(),
          model.covar_module.base_kernel.lengthscale.item(),
          model.likelihood.noise.item()
-         )
+         ))
     optimiser.step()
+
+# get into evaluation (predictive posterior) mode
+model.eval()
+likelihood.eval()
+
+# test points are regularly spaced along [0, 1]
+# make predictions by feeding model through likelihood
+with torch.no_grad(), gpytorch.settings.fast_pred_var():
+    test_x = torch.linspace(0, 1, 51)
+    observed_pred = likelihood(model(test_x))
+
+with torch.no_grad():
+    # initialise plot
+    f, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+    # get upper and lower confidence bounds
+    lower, upper = observed_pred.confidence_region()
+    # plot training data as black stars
+    ax.plot(train_x.numpy(), train_y.numpy(), 'k*')
+    # plot predictive means as blue line
+    ax.plot(test_x.numpy(), observed_pred.mean.numpy(), 'b')
+    # shade between the lower and upper confidence bounds
+    ax.fill_between(test_x.numpy(), lower.numpy(), upper.numpy(), alpha=0.5)
+    ax.set_ylim([-3, 3])
+    ax.legend(['Observed data', 'Mean', 'Confidence'])
+    
+    plt.show()
